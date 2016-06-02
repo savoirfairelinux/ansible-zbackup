f3c50492471560b24b6681ed6a712674ce957a3d
Virgil Dupras <virgil.dupras@savoirfairelinux.com>
2016-06-02 09:31:30 -0400

    Add --non-encrypted flag to restore script template

--- templates/getlatest.sh
+++ templates/getlatest.sh
@@ -2,4 +2,4 @@
 SCRIPT=`realpath $0`
 SCRIPTPATH=`dirname $SCRIPT`
 TIMESTAMP=`cat "${SCRIPTPATH}/latest_timestamp"`
-zbackup restore "${SCRIPTPATH}/repo/backups/archive-${TIMESTAMP}" {% if zbackup_compress_output %}| gzip -c{% endif %}
+zbackup --non-encrypted restore "${SCRIPTPATH}/repo/backups/archive-${TIMESTAMP}" {% if zbackup_compress_output %}| gzip -c{% endif %}
